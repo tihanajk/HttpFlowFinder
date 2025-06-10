7808d4be6679b056dd95883642a944c350ce4f01
Tihana Jurković-Kormo <tjkormo@span.eu>
2025-06-10 14:42:02 +0200

    assembly info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("HttpFlowFinder")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Span d.d.")]
+[assembly: AssemblyCompany("TJK")]
 [assembly: AssemblyProduct("HttpFlowFinder")]
-[assembly: AssemblyCopyright("Copyright © Span d.d. 2025")]
+[assembly: AssemblyCopyright("Copyright © TJK 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2025.6.2")]
+[assembly: AssemblyFileVersion("1.2025.6.2")]
